fefa5b41c1e825c7e663dee1ad9d3b3f57258c59
ana1487 <ana1487@my.londonmet.ac.uk>
2024-12-13 16:54:09 +0545

    looking at finding smallest number which is multiple of 1-20 numbers

---
+++ Euler_coding/algo_today.py
@@ -0,0 +1,18 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+def smallest_multiple(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = lcm(result, i)
+    return result
+
+# Find the smallest number divisible by all numbers from 1 to 20
+
+print(smallest_multiple(11))
+
